5d7165fe77f57c6072f8e8d1e16d2ba29a4bdfb4
WilMaye <wilvin.mayen@gmail.com>
2022-04-01 14:44:03 -0600

    Menu de inicio
    Iniciado la estructura de menu de inicio, siguiente paso el switch

---
+++ main proyecto.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std; 
+
+//Menu para las opciones	 (Wilvin)
+//a.	Insertar una nueva palabra
+//i.	Opcion para regresar el menu anterior
+//b.	Eliminar una palabra
+//i.	Opcion para regresar el menu anterior
+//c.	Buscar una palabra (pendiente de cómo se buscará) 
+//i.	Opcion para regresar el menu anterior
+//d.	Salir del programa
+
+
+int main()
+{
+	int m;
+	cout<< "------------Menu de trabajo de Traductor--------- \n \n";
+	cout<< "Ingrese el numero de la opcion segun se su preferencia. \n \n";
+	cout<<"1 Inserte una nueva palabra\n" ;
+	cout<<"2 Eliminar una palabra\n" ;
+	cout<<"3 Buscar una nueva palabra\n" ;
+	cout<<"4 Salir del Programa\n" ;
+
+//switch
+		
+}
